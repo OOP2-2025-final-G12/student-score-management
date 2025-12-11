2be89a639df6b8da4b78c59e0468ade65f3545fb
Haruimi <109784288+Haru-Sunflower@users.noreply.github.com>
2025-12-11 15:18:02 +0900

    製品->生徒
    製品->生徒に変更

--- models/product.py
+++ models/product.py
@@ -1,9 +1,12 @@
 from peewee import Model, CharField, DecimalField
 from .db import db
+from peewee import Check
 
 class Product(Model):
     name = CharField()
-    price = DecimalField()
+    price = DecimalField(constraints=[
+        Check('rating >= 1 AND rating <= 3')
+    ])
 
     class Meta:
         database = db
