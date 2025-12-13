db44455cb504b085df465b8023fedd17aff8d0a2
nasubi-dev <r916nis@gmail.com>
2025-12-14 02:02:33 +0900

    変数名の修正

--- models/user.py
+++ models/user.py
@@ -1,11 +1,10 @@
 from peewee import Model, CharField, IntegerField, Check
 from .db import db
 
+
 class User(Model):
     name = CharField()
-    age = IntegerField(constraints=[
-        Check('rating >= 1 AND rating <= 3')
-    ])
+    age = IntegerField(constraints=[Check("age >= 1 AND age <= 3")])
 
     class Meta:
         database = db
