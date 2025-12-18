abe1708059dfce3b87fb15df8abac518a28e487f
Haruimi <109784288+Haru-Sunflower@users.noreply.github.com>
2025-12-18 15:45:15 +0900

    成績一覧の修正
    成績入力の点数に0~100までの制限を加えました

--- models/order.py
+++ models/order.py
@@ -1,4 +1,4 @@
-from peewee import Model, ForeignKeyField, DateTimeField, CharField, IntegerField
+from peewee import Model, ForeignKeyField, DateTimeField, CharField, IntegerField, Check
 from .db import db
 from .user import User
 from .product import Product
@@ -9,7 +9,7 @@ class Order(Model):
     order_date = DateTimeField()
 
     type = CharField(max_length=20, default='未設定')
-    test_result = IntegerField(default=0)
+    test_result = IntegerField(default=0, constraints=[Check("test_result >= 0 AND test_result <= 100")])
 
     class Meta:
         database = db
